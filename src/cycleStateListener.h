0d5d9bb9a3be41f122163dd06c4e0fa78d2e4fd6
Wouter vd Heuvel <w.j.van.den.heuvel@xmsnet.nl>
2016-01-14 15:57:38 +0100

    Started with implementation of washing cycle task.

---
+++ src/cycleStateListener.h
@@ -0,0 +1,29 @@
+//**************************************
+//! Interface for classes that need information on the progression of a cycle.
+//!
+//! \authors
+//! 	- Wouter van den Heuvel
+//!
+//! \context
+//!		- part of TO6 assignment 2015-2016
+//**************************************
+
+#ifndef __CYCLE_STATE_LISTENER
+#define __CYCLE_STATE_LISTENER
+
+class cycleStateListener
+{
+public:
+	//! Called whenever the next step in a cycle begins execution.
+	virtual void cycleStateChanged(unsigned int totalSteps,
+		unsigned int currentStep)=0;
+	//! Called when the cycle is temporarily suspended. Upon resuming execution,
+	//! cycleStateChanged will be called.
+	virtual void cyclePaused()=0;
+	//! Called when the cycle ends. If the cycle ended by reaching the final
+	//! step, finished will be true. If the cycle got terminated before the end,
+	//! finished will be false.
+	virtual void cycleEnded(bool finished)=0;
+};
+
+#endif
