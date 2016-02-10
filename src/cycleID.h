569b35465ac140aae7f4904e10ad068b1babdaf6
Wouter van den Heuvel <w.j.van.den.heuvel@xmsnet.nl>
2016-02-10 10:38:10 +0100

    some work on WCT

---
+++ src/cycleID.h
@@ -0,0 +1,17 @@
+#ifndef __CYCLEID_H
+#define __CYCLEID_H
+
+#include <string>
+
+class cycleID
+{
+public:
+	cycleID(const std::string& cycleName, const std::string& owner);
+	cycleID(const cycleID& rhs);
+	bool operator== (const cycleID& lhs) const;
+	
+	std::string name;
+	std::string user;
+}
+
+#endif
